06a79b488841116b8b9df03dcf0f0e35cae45075
BorarosLorant <boraroslorant@gmail.com>
2025-05-15 06:06:16 +0200

    chapter-23

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,13 +18,15 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index")
 Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
 Route::get('/product/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
+
+Route::middleware('admin')->group(function(){
 Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
 Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name("admin.product.index");
 Route::post('/admin/products/store', 'App\Http\Controllers\Admin\AdminProductController@store')->name("admin.product.store");
 Route::delete('admin/products/{id}/delete','App\Http\Controllers\Admin\AdminProductController@delete')->name("admin.product.delete");
 Route::get('admin/products/{id}/edit','App\Http\Controllers\Admin\AdminProductController@edit')->name('admin.product.edit');
 Route::put('admin/products/{id}/update','App\Http\Controllers\Admin\AdminProductController@update')->name('admin.product.update');
-
+});
 
 Auth::routes();
 
